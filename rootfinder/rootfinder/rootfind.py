8729106f8d926ae8673a4907362e5649ee5d32f1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-14 14:41:39 -0400

    Add the TODO marker to the rootfind.py.

--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -1,5 +1,7 @@
 """Calculate the roots of a quadratic equation."""
 
+# TODO: Repair any defects in the function provided by this module
+
 
 def calculate_quadratic_equation_roots(a: float, b: float, c: float):
     """Calculate the roots of a quadratic equation."""
