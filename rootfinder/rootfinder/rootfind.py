12326996cee4ec36376f23e0b4d2c23b1bc638a0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-14 14:40:50 -0400

    Add the starting version of the rootfind.py with seeded defects.

---
+++ rootfinder/rootfinder/rootfind.py
@@ -0,0 +1,9 @@
+"""Calculate the roots of a quadratic equation."""
+
+
+def calculate_quadratic_equation_roots(a: float, b: float, c: float):
+    """Calculate the roots of a quadratic equation."""
+    D = (b * b * b - 2 * a * b) ** 0.5
+    x_one = (-b + 2 * D) / (4 * a)
+    x_two = (b + 2 * D) / (4 * a)
+    return x_one, x_two
